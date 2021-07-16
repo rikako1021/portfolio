68f2938696fb4628a8598e15648d79d60e806317
rikako1021 <omote.rikako.19@shizuoka.ac.jp>
2021-07-16 22:41:11 +0900

    About.js修正

--- src/components/About.js
+++ src/components/About.js
@@ -2,8 +2,34 @@ import React from 'react'
 
 function About() {
  return (
-   <div>
-     About page
+   <div className="about">
+       <h1>About Me</h1>
+       <div className="profile">
+           <h2>Profile</h2>
+           <p>あああああああああああ</p>
+       </div>
+       <div className="content">
+           <div className="skill">
+            <h2>Programming Skill</h2>
+            <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>Javascript</li>
+                <li>PHP</li>
+                <li>Golang</li>
+            </ul>
+            <h2>Tools</h2>
+            <ul>
+                <li>tool1</li>
+                <li>tool2</li>
+                <li>tool3</li>
+            </ul>
+           </div>
+           <div className="experience">
+               <h2>Experience</h2>
+                <p>いいいいいいいいいいいいい</p>
+           </div>
+       </div>
    </div>
  )
 }
