c8f004938d0f0401714eeed247d16a99d7dcc3bd
rikako1021 <omote.rikako.19@shizuoka.ac.jp>
2021-07-16 21:00:50 +0900

    router追加

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Swicth} from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Swicth>
+      <Route component={Home} path='/' exact />
+      <Route component={About} path='/about' />
+    </Swicth>
+    </BrowserRouter>
   );
 }
 
