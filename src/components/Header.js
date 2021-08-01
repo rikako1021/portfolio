cb7fd38d608fd3dbf589b3b19b0fb5a01b99b6a9
rikako1021 <omote.rikako.19@shizuoka.ac.jp>
2021-08-02 00:30:13 +0900

    ハンバーガーメニュー実装

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,35 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import image from '../image/menu5.png'
 
-function Header() {
+
+/*const navButton = document.getElementById('#nav-toggle');
+ navButton.addEventListener('click', function() {
+     navButton.classList.add('open')
+    }) */
+
+function Header() { 
  return (
-     <header>
-         <h1>
-             <NavLink to="/" axact="true">Portfolio</NavLink>
-         </h1>
-         <nav>
-             <ul>
-                 <li><NavLink to="/about">About Me  </NavLink></li>
-                 <li><NavLink to="/Product">Product  </NavLink></li>
-                 <li><NavLink to="/contact">Contact  </NavLink></li>
-             </ul>
-         </nav>
-     </header>
+     <header id="head_wrap">
+        <h1 className="logo">
+            <NavLink to="/" axact="true">Portfolio</NavLink>
+        </h1>
+        <nav>
+        <div id="open" classNmae="btn">
+             <a href="#g_menu"><img src={image} alt="menu" width="40%" /></a>
+        </div>
+        <div id="g_menu">
+        <div id="close" className="btn">
+            <a href="#">×</a>
+        </div>
+            <ul>
+            <li><NavLink to="/about">About Me  </NavLink></li>
+            <li><NavLink to="/Product">Product  </NavLink></li>
+            <li><NavLink to="/contact">Contact  </NavLink></li>
+            </ul>
+        </div>
+        </nav>
+     </header>    
 )
- }
-
+}
 export default Header
